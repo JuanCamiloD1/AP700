c80ff7d59d196cbb5c8618fea1149c4924b6bb46
JuanCamiloD1 <juancamilodiazbasto@gmail.com>
2020-07-20 15:31:37 -0500

    added commands

---
+++ comandos/kick.js
@@ -0,0 +1,13 @@
+const Discord = require("discord.js");
+module.exports = (client, message, args) => { 
+    let mencionado = message.mentions.users.first();
+    let razon = args.slice(1).join(' ');
+  
+    if(!mencionado) return message.reply('No ha mencionando a ningún miembro.');
+    if(!razon) return message.channel.send('Escriba una razón del uso de kick.');
+  
+    message.guild.member(mencionado).kick(razon);
+    message.channel.send(`**${mencionado.username}**, fue expulsado del servidor, razón: ${razon}.`);
+  
+  }
+  
